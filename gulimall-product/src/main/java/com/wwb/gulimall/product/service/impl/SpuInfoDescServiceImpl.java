8927faf8626a33162258f7bd83ac6801dd21825c
weiweibin <571431755@qq.com>
2020-09-09 21:57:27 +0800

    项目初始结构创建

---
+++ gulimall-product/src/main/java/com/wwb/gulimall/product/service/impl/SpuInfoDescServiceImpl.java
@@ -0,0 +1,29 @@
+package com.wwb.gulimall.product.service.impl;
+
+import org.springframework.stereotype.Service;
+import java.util.Map;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.wwb.common.utils.PageUtils;
+import com.wwb.common.utils.Query;
+
+import com.wwb.gulimall.product.dao.SpuInfoDescDao;
+import com.wwb.gulimall.product.entity.SpuInfoDescEntity;
+import com.wwb.gulimall.product.service.SpuInfoDescService;
+
+
+@Service("spuInfoDescService")
+public class SpuInfoDescServiceImpl extends ServiceImpl<SpuInfoDescDao, SpuInfoDescEntity> implements SpuInfoDescService {
+
+    @Override
+    public PageUtils queryPage(Map<String, Object> params) {
+        IPage<SpuInfoDescEntity> page = this.page(
+                new Query<SpuInfoDescEntity>().getPage(params),
+                new QueryWrapper<SpuInfoDescEntity>()
+        );
+
+        return new PageUtils(page);
+    }
+
+}
